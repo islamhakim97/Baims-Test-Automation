d0fba799dcb9e2964e7b49a59ab206bdfd2ab430
Islam Abd El Hakim <33816681+islamhakim97@users.noreply.github.com>
2022-05-02 22:39:38 +0200

    Inet The First Commit&Push To Remote Repo Automate Baims.

---
+++ Baims/src/main/java/Baims/Baims/App.java
@@ -0,0 +1,13 @@
+package Baims.Baims;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
